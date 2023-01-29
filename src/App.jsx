2a421c09ec058fafd7815d80fb7a7faeb8b4b379
venutti <lvenutti@fi.uba.ar>
2023-01-29 19:47:51 -0300

    Receiving new titles

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
+
 export default function App() {
-  return <div>App</div>;
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    console.log("Create a book with the title", title);
+  };
+
+  return (
+    <div>
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 }
